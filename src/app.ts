677118c49d1acddd3d58e86afa1cb62884df6bcb
Pedro Santos <pedrocarlos.ti@gmail.com>
2024-10-16 17:46:01 +0100

    add .env

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,41 @@
-import http from 'http';
-import express from 'express';
-import { Server, Socket } from 'socket.io';
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+
+dotenv.config();
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const io: Server = new Server(server, {
   cors: {
-    origin: 'http://192.168.1.75:5173',
-    methods: ['GET', 'POST'],
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
   },
 });
 
 const PORT: number = 3000;
 
-let currentContent: string = '';
+let currentContent: string = "";
 
 // Socket.IO connection
-io.on('connection', (socket: Socket) => {
-  console.log('A user connected');
+io.on("connection", (socket: Socket) => {
+  console.log("A user connected");
 
   // Send the current content to the newly connected user
-  socket.emit('initial_content', currentContent);
+  socket.emit("initial_content", currentContent);
 
   // Listen for content sent from clients
-  socket.on('send_content', (content: string) => {
+  socket.on("send_content", (content: string) => {
     // Update the stored content
     currentContent = content;
     // Broadcast the content to all clients, including the sender
-    io.emit('receive_content', content);
+    io.emit("receive_content", content);
   });
 
   // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
   });
 });
 
